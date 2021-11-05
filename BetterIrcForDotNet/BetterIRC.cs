4884c067a2de5dd833b1382c8966e75c0e983e2a
Krutonium <PFCKrutonium@gmail.com>
2021-11-05 06:24:29 -0400

    A first commit. It can connect to an IRC server, send a message, recieve a message, and disconnect. 99% of what you would want.

---
+++ BetterIrcForDotNet/BetterIRC.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+//https://cerkit.com/2019/01/10/create-a-simple-irc-bot-with-net-core/
+// ReSharper disable MethodHasAsyncOverload
+//https://datatracker.ietf.org/doc/html/rfc2812
+
+namespace BetterIrcForDotNet
+{
+    public class BetterIRC
+    {
+        private static class Controls
+        {
+            internal static TcpClient client = new TcpClient();
+            internal static StreamWriter writer;
+            internal static StreamReader reader;
+        }
+
+        public List<string> ConnectedChannels = new List<string>();
+        public bool connected = false;
+        public async Task ConnectToIRCAsync(string URL, int Port, string Username, string Password = "", bool verbose = false)
+        {
+            Controls.client.Connect(URL, Port);
+            Controls.reader = new StreamReader(Controls.client.GetStream());
+            Controls.writer = new StreamWriter(Controls.client.GetStream()) { NewLine = "\r\n", AutoFlush = true };;
+            Controls.writer.WriteLineAsync($"USER {Username} * 8 BetterIRC");
+            Controls.writer.WriteLineAsync($"NICK {Username}"); 
+            Controls.writer.FlushAsync();
+            RaiseConnectedMessage(Controls.client.Connected);
+            while (Controls.client.Connected)
+            {
+                connected = Controls.client.Connected;
+                var data = Controls.reader.ReadLine();
+                if (data != null)
+                {
+                    var d = data.Split(' ');
+                    if (verbose)
+                    {
+                        Console.WriteLine(data);
+                    }
+                    if (d[0] == "PING")
+                    {
+                        //Handle Ping
+                        //Doing a replace means that any tokens the server attach stay intact.
+                        Controls.writer.WriteLine(data.Replace("PING", "PONG"));
+                    }
+
+                    if (d.Length > 1)
+                        switch (d[1])
+                        {
+                            case "376":
+                                RaiseReady(true);
+                                break;
+                            case "001":
+                                break;
+                            case "PRIVMSG":
+                                string p = d[0].Split('!')[0];
+                                var msgbeta = data.Split(':');
+                                var msg = msgbeta[^1];
+                                RaiseChatMessage(p, d[2],msg);
+                                break;
+                            
+                        }
+                }
+            }
+            RaiseConnectedMessage(Controls.client.Connected);
+            connected = Controls.client.Connected;
+        }
+
+        public void ConnectToChannel(string Channel)
+        {
+            string toConnectTo;
+            if (!Channel.StartsWith("#"))
+            {
+                toConnectTo = $"#{Channel}";
+            }
+            else
+            {
+                toConnectTo = Channel;
+            }
+            ConnectedChannels.Add(toConnectTo);
+            Controls.writer.WriteLine($"JOIN {toConnectTo}");
+            Console.WriteLine($"Joined Channel {toConnectTo}");
+        }
+
+        public void Disconnect(string message)
+        {
+            Controls.writer.WriteLine($"QUIT {message}");
+            RaiseReady(false);
+            RaiseConnectedMessage(false);
+            connected = false;
+        }
+
+        /// <summary>
+        /// Raised when a Chat Message is recieved.
+        /// </summary>
+        /// <param name="user">Username of the Sender if applicable</param>
+        /// <param name="channel">Channel the message was sent from</param>
+        /// <param name="message">The message that was sent</param>
+        private void RaiseChatMessage(string user, string channel, string message)
+        {
+            if (MessageRecieved != null)
+            {
+                //OverdrawnArgs args = new OverdrawnArgs();
+                var args = new ChatMessage();
+                args.Author = user.Replace(":", "");
+                args.Author = "@" + args.Author;
+                args.Message = message;
+                args.Channel = channel;
+                MessageRecieved(this, args);
+            }
+        }
+        /// <summary>
+        /// Tells the external application if we are connected or not.
+        /// </summary>
+        /// <param name="connected">Connection Status - True is Connected.</param>
+        private void RaiseConnectedMessage(bool connected)
+        {
+            if (ConnectionStatus != null) ConnectionStatus(this, connected);
+        }
+
+        public void SendMessage(string target, string message)
+        {
+            if (target.StartsWith("#"))
+            {
+                if (ConnectedChannels.Contains(target))
+                {
+                    Controls.writer.WriteLine($"PRIVMSG {target} :{message}");
+                }
+            }
+            else
+            {
+                //Must be a PM
+                Controls.writer.WriteLine($"PRIVMSG {target} :{message}");
+            }
+        }
+        private void RaiseReady(bool ready)
+        {
+            if (IRC_Ready != null) IRC_Ready(this, ready);
+        }
+
+        public event EventHandler<bool> IRC_Ready;
+        public event EventHandler<bool> ConnectionStatus;
+        
+        public event EventHandler<ChatMessage> MessageRecieved;
+        public class ChatMessage : EventArgs
+        {
+            public string Author { get; set; }
+            public string Message { get; set; }
+            public string Channel { get; set; }
+        }
+    }
+
+}
---
+++ Example/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using BetterIrcForDotNet;
+
+namespace Example
+{
+    class Program
+    {
+        static BetterIRC irc = new BetterIRC();
+        static async Task Main(string[] args)
+        {
+            Random rand = new Random();
+            irc.MessageRecieved += IrcOnMessageRecieved;
+            irc.ConnectionStatus += IrcOnConnectionStatus;
+            irc.IRC_Ready += IrcOnIRC_Ready;
+            Task.Run(() =>
+            {
+                irc.ConnectToIRCAsync("irc.libera.chat", 8000, $"Random{rand.Next(0,1000)}", "");
+            });
+            //We can do things once it's ready.
+            while (irc.ConnectedChannels.Count == 0)
+            {
+                //You should make sure you're connected to channels before doing anything with them.
+                System.Threading.Thread.Sleep(500);
+            }
+            irc.SendMessage(irc.ConnectedChannels[0], "This is a message indicating this code ran!");
+            irc.Disconnect("Boom"); //Doesn't seem to work presently.
+            Console.ReadKey();
+            
+        }
+
+        private static void IrcOnIRC_Ready(object? sender, bool e)
+        {
+            if (e == true)
+            {
+                //We need to join a channel for the bot to become usable beyond PM's.
+                irc.ConnectToChannel("#krubot_test");
+            }
+            else
+            {
+                //We probably just DC'd.
+                Console.WriteLine("No longer ready.");
+            }
+
+        }
+
+        private static void IrcOnConnectionStatus(object? sender, bool e)
+        {
+            //THIS ONLY INDICATES IF WE HAVE A TCP CONNECTION
+            //NOT IF WE CAN SEND MESSAGES!
+            switch (e)
+            {
+                case true:
+                    Console.WriteLine("Connected to the server");
+                    break;
+                case false:
+                    Console.WriteLine("Disconnected from the Server");
+                    break;
+            }
+        }
+
+        private static void IrcOnMessageRecieved(object? sender, BetterIRC.ChatMessage e)
+        {
+            Console.WriteLine($"{e.Channel} - {e.Author}: {e.Message}");
+            irc.SendMessage(e.Channel, $"Hello {e.Author}!");
+        }
+    }
+}
